7ba7e4475d5238ba5b11aa00152d8e94dae0f7c9
KFIR HEMO <KFIRH@rafael.co.il>
2023-12-20 14:48:34 +0200

    finished models

--- webServer1/Models/Manager.cs
+++ webServer1/Models/Manager.cs
@@ -6,6 +6,6 @@ public class Manager
 	public int Id { get; set; }
     public string? ManagerId { get; set; }
     public string? Password { get; set; }
-
+    public List<Appointment>? Appointments { get; set; }
 }
 
